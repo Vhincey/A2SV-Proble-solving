5711e91439ed47d4aa1b13a14cba198b1fe4823b
Vhincey <87284470+Vhincey@users.noreply.github.com>
2023-09-14 21:06:12 +0000

    Update Implement Stack using Queues.py

--- Implement Stack using Queues.py
+++ Implement Stack using Queues.py
@@ -21,6 +21,56 @@ class MyStack:
         if self.size == 0:
           return True
         return False
+# Method 2
+
+class Node():
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+class MyStack(object):
+
+    def __init__(self):
+        self.size = 0
+        self.head = None
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        node = Node(x)
+
+        node.next = self.head
+        self.head = node
+        self.size += 1
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        if self.size == 0:
+          return
+        else:
+          save = self.head.val
+          self.head = self.head.next
+          self.size -= 1
+          return save
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if self.size:
+          return self.head.val
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return self.size == 0
+          
+        
 
 
 # Your MyStack object will be instantiated and called as such:
