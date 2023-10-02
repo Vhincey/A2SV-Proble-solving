0374698db3904148ba3883753a4561a51a300982
Vhincey <87284470+Vhincey@users.noreply.github.com>
2023-10-02 21:29:35 +0000

    Update Contains Duplicate.py

--- Contains Duplicate.py
+++ Contains Duplicate.py
@@ -1,12 +1,41 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
-      num_counts = {}  
+       # Brute Force
+      # n = len(nums)
+      # for i in range(n - 1):
+      #     for j in range(i + 1, n):
+      #         if nums[i] == nums[j]:
+      #             return True
+      # return False
+
+      # Time complexity of n(logn)
+      # n = len(nums)
+      # nums.sort()
+
+      # for i in range(n - 1):
+      #   if nums[i] == nums[i + 1]:
+      #     return True
+      # return False
+
+      # Time complexity of O(n) with space of O(n)
+      # num_counts = {}  
+
+      # for num in nums:
+      #   if num in num_counts:
+      #       return True 
+      #   else:
+      #       num_counts[num] = 1
+
+      # return False 
+
+      #  Using Hashsets
+      hashset = set()
 
       for num in nums:
-        if num in num_counts:
-            return True 
-        else:
-            num_counts[num] = 1
+        if num in hashset:
+          return True
+        hashset.add(num)
+      return False
 
-      return False 
+     
