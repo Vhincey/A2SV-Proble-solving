78059b76e8f819729f0d00102d47602ef9d78301
Vhincey <87284470+Vhincey@users.noreply.github.com>
2023-10-04 22:41:31 +0000

    Create Group Anagrams.py

---
+++ Group Anagrams.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hmap = {}
+
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word in hmap:
+                hmap[sorted_word].append(word)
+            else:
+                hmap[sorted_word] = [word]
+        return list(hmap.values())
