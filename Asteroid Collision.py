1c380af23f4e941a117adebc8ac5504b46bdeb32
Vhincey <87284470+Vhincey@users.noreply.github.com>
2023-10-25 00:00:06 +0000

    Create Asteroid Collision.py

---
+++ Asteroid Collision.py
@@ -0,0 +1,35 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        # stack = []
+        
+        # for asteroid in asteroids:
+        #     while stack and asteroid < 0 and stack[-1] > 0:
+        #         if abs(stack[-1]) > abs(asteroid):
+        #             asteroid = 0  
+        #         elif abs(stack[-1]) == abs(asteroid):
+        #             stack.pop()  
+        #             asteroid = 0
+        #         else:
+        #             stack.pop()  
+                
+        #     if asteroid != 0:
+        #         stack.append(asteroid)
+
+        # return stack
+    
+        stack = []
+        
+        for asteroid in asteroids:
+            while stack and asteroid < 0 and stack[-1] > 0:
+                if stack[-1] < abs(asteroid):
+                    stack.pop()
+                    continue
+                elif stack[-1] == abs(asteroid):
+                    stack.pop()
+                break
+            else:
+                stack.append(asteroid)
+
+        return stack
+
+
