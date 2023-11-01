b520768a590af5b74d7e4109a2d58778d10a3db5
Vhincey <87284470+Vhincey@users.noreply.github.com>
2023-11-01 21:10:36 +0000

    Create Permutations.py

---
+++ Permutations.py
@@ -0,0 +1,26 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def is_valid_state(state):
+            if len(state) == len(nums):
+                return True
+
+        def get_candidates(state):
+            if not state:
+                return nums
+            else:
+                return [num for num in nums if num not in state]
+
+        def search(state, solutions):
+            if is_valid_state(state):
+                solutions.append(state.copy())
+                return
+
+            for candidate in get_candidates(state):
+                state.append(candidate)
+                search(state, solutions)
+                state.remove(candidate)
+
+        solutions = []
+        state = []
+        search(state, solutions)
+        return solutions
