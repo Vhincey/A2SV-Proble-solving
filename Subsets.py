73bbb2e35b53b0552a2fc2cacfcd03b483f35bc0
Vhincey <87284470+Vhincey@users.noreply.github.com>
2023-11-06 15:13:47 +0000

    Update Subsets.py

--- Subsets.py
+++ Subsets.py
@@ -1,18 +1,17 @@
- def is_valid_state(state):
-            if len(state) <= len(nums):
-                return True
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def is_valid_state(state):
+            return len(state) <= len(nums)
 
         def get_candidates(state):
-            lst = []
             if not state:
                 return nums
             else:
-                lst = [num for num in nums if num > max(state)]
-            return lst
+               return [num for num in nums if num > max(state)]
 
         def search(state, solutions):
             if is_valid_state(state):
-                solutions.add(tuple(state.copy()))
+                solutions.append(tuple(state.copy()))
                 # return
 
             for candidate in get_candidates(state):
@@ -20,7 +19,7 @@
                 search(state, solutions)
                 state.remove(candidate)
 
-        solutions = set()
+        solutions = []
         state = set()
         search(state, solutions)
         return solutions
