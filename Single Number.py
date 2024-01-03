9593e30246d950fabe46a5133bf6cde241e9020c
Vhincey <87284470+Vhincey@users.noreply.github.com>
2024-01-03 23:01:42 +0000

    Create Single Number.py

---
+++ Single Number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        else:
+            dict = {}
+           
+            for num in nums:
+                if num not in dict:
+                   dict[num] = 1
+                else:
+                    dict[num] += 1
+
+            for num, count in dict.items():
+                if count == 1:
+                    return num          
+
+                
+
+                    
