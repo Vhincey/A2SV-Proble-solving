2f27604b8f8ace60071942ff20d0c33cde52185c
Vhincey <87284470+Vhincey@users.noreply.github.com>
2024-01-05 22:37:32 +0000

    Create Best Time to Buy and Sell Stock II.py

---
+++ Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        
+        for i in range(1, len(prices)):
+            max_profit += max(prices[i] - prices[i - 1],0)
+        
+        return max_profit
+
+
+# Method 2
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+        
+        return max_profit
