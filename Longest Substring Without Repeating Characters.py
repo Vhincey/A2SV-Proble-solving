ae618d8a11344b8305bb1467098f463e0d779a53
Vhincey <87284470+Vhincey@users.noreply.github.com>
2024-01-17 17:56:00 +0000

    Update and rename Longest Substring Without Repeating Characters to Longest Substring Without Repeating Characters.py

--- Longest Substring Without Repeating Characters.py
+++ Longest Substring Without Repeating Characters.py
@@ -17,4 +17,18 @@ class Solution(object):
             longest = max(longest, right - left + 1)
 
         return longest
-        
+
+
+# Method 2
+        char_index_map = {}
+        start = 0
+        max_length = 0
+
+        for end in range(len(s)):
+            if s[end] in char_index_map and char_index_map[s[end]] >= start:
+                start = char_index_map[s[end]] + 1
+
+            char_index_map[s[end]] = end
+            max_length = max(max_length, end - start + 1)
+
+        return max_length
